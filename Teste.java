c9332ae26a70a7a2d1f161d1f18f6a543daa476d
joaohenriquebf <joao.fernandes.002@acad.pucrs.br>
2019-10-07 22:16:54 -0300

    Create Teste.java

---
+++ Teste.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+@Test
+void testIntegracao() throws IOException {
+AA.main(null);
+AB.main(null);
+}
+
+}
