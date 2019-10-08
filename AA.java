cfdd20c77de80de420dbe38391b19af79e05b24f
joaohenriquebf <joao.fernandes.002@acad.pucrs.br>
2019-10-07 21:52:14 -0300

    Create AA.java

---
+++ AA.java
@@ -0,0 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class AA {
+
+
+public static void main(String[] args) throws IOException {
+int i, n;
+
+FileWriter arq = new FileWriter("h:\\TrabProd.txt");
+PrintWriter gravarArq = new PrintWriter(arq);
+
+for (i=1; i<=10; i++) {
+gravarArq.printf(i+":"+i*i+":"+i*i*i);
+gravarArq.printf("\n");
+}
+
+arq.close();
+
+System.out.printf("\nGravado com sucesso em \"h:\\TrabProd.txt\".\n");
+}
+
+}
